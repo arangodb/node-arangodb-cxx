b82aa60c9a66c8b19a1ea20a4814af0b409536fa
Jan Christoph Uhde <Jan@UhdeJC.com>
2017-03-15 14:12:19 +0100

    let alan try to use npm install

--- src/node_upstream.h
+++ src/node_upstream.h
@@ -22,8 +22,12 @@
 #pragma once
 
 // debian sid
-#include <nodejs/deps/v8/include/v8.h>
-#include <nodejs/src/node.h>
+//#include <nodejs/deps/v8/include/v8.h>
+//#include <nodejs/src/node.h>
+//#include <nan.h>
+
+#include <v8.h>
+#include <node.h>
 #include <nan.h>
 
 #include <fuerte/fuerte.h>
